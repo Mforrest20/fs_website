c2fc335caede7b6a62f5b3bc4bad71c77bba5633
steven <stevenesammon@hotmail.com>
2020-10-21 20:11:21 -0400

    Added GET routes and hbs files for 5 pages

--- controller/html-router.js
+++ controller/html-router.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res){
 
 });
 
+router.get('/services', function(req, res){
+    res.render('services', {subtitle:"Services"});
+
+});
+
+router.get('/projects', function(req, res){
+    res.render('projects', {subtitle:"Projects"});
+
+});
+
+router.get('/about', function(req, res){
+    res.render('about', {subtitle:"About Us"});
+
+});
+
+router.get('/contact', function(req, res){
+    res.render('contact', {subtitle:"Contact"});
+
+});
+
 module.exports = router;
